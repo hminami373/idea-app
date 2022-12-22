ce2baae47aecfb0b563f31ea11d514060537a0f8
hminami373 <south.s.373.n.y@gmail.com>
2022-12-22 14:35:45 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  root to: "rooms#index"
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 end
+
+private
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :introduction])
+end
+
