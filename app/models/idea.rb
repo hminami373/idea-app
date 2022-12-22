a93d4a876fd98cdcce9dc99f9efc72fc1ad0f7f0
hminami373 <south.s.373.n.y@gmail.com>
2022-12-22 23:28:33 +0900

    Active Storageの導入

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  has_one_attached :image
 
   validates :image, :idea, presence: true
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   belongs_to :user
   has_many :ideas
+  has_one_attached :image
 
   validates :image, :theme, :theme_info, presence: true
 
